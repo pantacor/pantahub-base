184ed20620d59bc75816a71370cbd63cbe4e2236
Alexander Sack <asac129@gmail.com>
2017-10-23 01:36:04 +0200

    add URLCleanMiddleware

---
+++ utils/urlclean-mw.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+// RecorderMiddleware keeps a record of the HTTP status code of the response,
+// and the number of bytes written.
+// The result is available to the wrapping handlers as request.Env["STATUS_CODE"].(int),
+// and as request.Env["BYTES_WRITTEN"].(int64)
+type URLCleanMiddleware struct{}
+
+// MiddlewareFunc makes RecorderMiddleware implement the Middleware interface.
+func (mw *URLCleanMiddleware) MiddlewareFunc(h rest.HandlerFunc) rest.HandlerFunc {
+	return func(w rest.ResponseWriter, r *rest.Request) {
+		var err error
+		log.Println("Cleaning: " + r.URL.Path)
+		urlPath := strings.TrimSuffix(r.URL.Path, "/")
+
+		r.URL, err = url.Parse(urlPath)
+
+		log.Println("Cleaned: " + r.URL.Path)
+		if err != nil {
+			rest.Error(w, "Error cleaning trailing / from path", http.StatusInternalServerError)
+		}
+
+		// call the handler
+		h(w, r)
+	}
+}
