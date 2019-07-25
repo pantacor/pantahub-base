9b705543aeb0f4bbec6a5b1597282d778d9308cb
Sergio Marin <higher.vnf@gmail.com>
2019-07-25 21:44:25 +0000

    move to rsa pub/priv key jwt token signing; adopt proper fork github.com/pantacor/go-json-rest-middleware-jwt

--- trails/trails-api.go
+++ trails/trails-api.go
@@ -68,15 +68,14 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-
 type PvrRemote struct {
-        RemoteSpec         string   `json:"pvr-spec"`         // the pvr remote protocol spec available
-        JsonGetUrl         string   `json:"json-get-url"`     // where to pvr post stuff
-        JsonKey            string   `json:"json-key"`         // what key is to use in post json [default: json]
-        ObjectsEndpointUrl string   `json:"objects-endpoint"` // where to store/retrieve objects
-        PostUrl            string   `json:"post-url"`         // where to post/announce new revisions
-        PostFields         []string `json:"post-fields"`      // what fields require input
-        PostFieldsOpt      []string `json:"post-fields-opt"`  // what optional fields are available [default: <empty>]
+	RemoteSpec         string   `json:"pvr-spec"`         // the pvr remote protocol spec available
+	JsonGetUrl         string   `json:"json-get-url"`     // where to pvr post stuff
+	JsonKey            string   `json:"json-key"`         // what key is to use in post json [default: json]
+	ObjectsEndpointUrl string   `json:"objects-endpoint"` // where to store/retrieve objects
+	PostUrl            string   `json:"post-url"`         // where to post/announce new revisions
+	PostFields         []string `json:"post-fields"`      // what fields require input
+	PostFieldsOpt      []string `json:"post-fields-opt"`  // what optional fields are available [default: <empty>]
 }
 
 type TrailsApp struct {
