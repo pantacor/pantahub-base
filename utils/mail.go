8bad71d63749e1a293ce9da3bcd85da71c44ca55
Sergio Marin <sergio.marin@pantacor.com>
2022-01-25 13:38:28 -0300

    fix(mails): attach only necessary images

--- utils/mail.go
+++ utils/mail.go
@@ -202,42 +202,6 @@ func addMedias(message *mailgun.Message) error {
 		return err
 	}
 
-	twitterPng, err := base64.StdEncoding.DecodeString(ImageTwitter)
-	if err != nil {
-		log.Println("error:", err)
-		return err
-	}
-
-	linkedinPng, err := base64.StdEncoding.DecodeString(ImageLinkedin)
-	if err != nil {
-		log.Println("error:", err)
-		return err
-	}
-
-	rdPng, err := base64.StdEncoding.DecodeString(ImageRd)
-	if err != nil {
-		log.Println("error:", err)
-		return err
-	}
-
-	ruPng, err := base64.StdEncoding.DecodeString(ImageRu)
-	if err != nil {
-		log.Println("error:", err)
-		return err
-	}
-
-	redditPng, err := base64.StdEncoding.DecodeString(ImageReddit)
-	if err != nil {
-		log.Println("error:", err)
-		return err
-	}
-
 	message.AddReaderInline("logo.png", ioutil.NopCloser(bytes.NewReader(logoPng)))
-	message.AddReaderInline("twitter.png", ioutil.NopCloser(bytes.NewReader(twitterPng)))
-	message.AddReaderInline("linkedin.png", ioutil.NopCloser(bytes.NewReader(linkedinPng)))
-	message.AddReaderInline("reddit.png", ioutil.NopCloser(bytes.NewReader(redditPng)))
-	message.AddReaderInline("rd.png", ioutil.NopCloser(bytes.NewReader(rdPng)))
-	message.AddReaderInline("ru.png", ioutil.NopCloser(bytes.NewReader(ruPng)))
-
 	return nil
 }
